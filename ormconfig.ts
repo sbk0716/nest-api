3b4bf325db8ae35f20b9ecac29636d8807471b00
sbk0716 <uhey0716@gmail.com>
2022-02-24 07:47:08 +0900

    [WIP]add workflow for node-ci

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,23 @@
+const nodeEnv = process.env.NODE_ENV || 'development';
+console.info('process.env.NODE_ENV= ', nodeEnv);
 const dbHostName = process.env.POSTGRES_HOST || 'app-db';
 const dbPort = process.env.POSTGRES_PORT || 5432;
 const dbUserName = process.env.POSTGRES_USER || 'admin';
 const dbPassword = process.env.POSTGRES_PASSWORD || 'dummypassword';
 let dbName = process.env.DB_NAME || 'coredb';
-// let entities = 'dist/**/*.entity{.ts,.js,.js.map}';
-// let migrations = 'dist/src/migrations/*{.ts,.js,.js.map}';
+let entities = 'dist/**/*.entity{.ts,.js,.js.map}';
+let migrations = 'dist/src/migrations/*{.ts,.js,.js.map}';
+
 if (process.env.MODE === 'TEST') {
   dbName = 'testdb';
-  // entities = 'src/**/*.entity{.ts,.js}';
-  // migrations = 'src/migrations/*{.ts,.js,.js.map}';
+  entities = 'src/**/*.entity{.ts,.js}';
+  migrations = 'src/migrations/*{.ts,.js,.js.map}';
 }
-const entities = 'src/**/*.entity{.ts,.js}';
-const migrations = 'src/migrations/*{.ts,.js,.js.map}';
+
+// if (nodeEnv === 'development') {
+//   entities = 'src/**/*.entity{.ts,.js}';
+//   migrations = 'src/migrations/*{.ts,.js,.js.map}';
+// }
 
 const connectionOptions = {
   type: 'postgres',
@@ -24,6 +30,16 @@ const connectionOptions = {
   entities: [entities],
   migrations: [migrations],
 };
+
+if (process.env.DB_SECRETS) {
+  const raw = process.env.DB_SECRETS;
+  const dbSecrets = JSON.parse(raw);
+  connectionOptions.host = process.env.DB_ENDPOINT;
+  connectionOptions.port = process.env.DB_PORT;
+  connectionOptions.username = dbSecrets.dbUserName;
+  connectionOptions.password = dbSecrets.dbPassword;
+  connectionOptions.database = dbSecrets.dbName;
+}
 
 console.info('####################');
 console.info(connectionOptions);
